77bf23fbb7d00ad3cb53c0a4630784ddcafd4c1c
Kiyomitsu Nakao <brbr091429@yahoo.co.jp>
2018-12-18 22:01:55 +0900

    clear error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,40 @@
 class PostsController < ApplicationController
   def index
     @title='Posts Index'
-    @post = Post.all
+    @posts = Post.all
     # @post = Post.all.order(created_at: :desc)
   end
 
   def show
     @title='Show id'
-    @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(content: params[:content])
     @post.save
-    redirect_to('posts/index')
+    redirect_to("/posts")
   end
 
   def edit
     @title='Edit it'
-    @post = Post.find_by(:id)
+    @post = Post.find_by(id: params[:id])
   end
 
   def update
-    @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
     @post.save
-    redirect_to('posts/index')
+    redirect_to('posts')
   end
 
   def destroy
-    @post = Post.find_by(:id)
+    @post = Post.find_by(id: params[:id])
     @post.destroy
-    redirect_to('post/index')
+    redirect_to("/posts")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get 'posts'=>'posts#index'
   get 'posts/new' => 'posts#new'
   post 'posts/create' => 'posts#create'
-  get 'posts/:id'=>'posts#show'
-  get 'posts/:id/edit' => 'posts#edit'
-  post 'posts/:id/update' => 'posts#update'
-  post 'post/:id/destroy' => 'posts#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "posts/:id"=>'posts#show'
+  get "posts/:id/edit" => 'posts#edit'
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id"=>"posts#show"
 end
